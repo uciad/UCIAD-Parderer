194b458b6ca5d7715b037d0f6389d9f35adec11f
Salman Elahi <salman_mail@yahoo.com>
2011-12-22 17:37:47 +0000

    SE 22/12/2011

--- src/uk/ac/open/kmi/uciad/helper/ActorAgent.java
+++ src/uk/ac/open/kmi/uciad/helper/ActorAgent.java
@@ -2,8 +2,6 @@ package uk.ac.open.kmi.uciad.helper;
 
 public class ActorAgent {
 	private String actorAgentId;
-        
-        private String test = "test";
 
 	public void setActorAgentId(String actorAgentId) {
 		this.actorAgentId = actorAgentId;
